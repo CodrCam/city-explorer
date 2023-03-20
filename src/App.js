c07791443c96669a05fde417c80caec7dbf49b31
Cameron Griffin <cambackcountry@gmail.com>
2023-03-20 15:29:58 -0700

    deploy preview

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import axios from 'axios';
+import CityInput from './CityInput';
+import ErrorMessage from './ErrorMessage';
+import CityData from './CityData';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      city: '',
+      cityData: {},
+      error: false,
+      errorMessage: ''
+    }
+  }
+
+  handleCityInput = (event) => {
+    this.setState({
+      city: event.target.value
+    })
+  }
+
+  getCityData = async (event) => {
+    event.preventDefault();
+
+    try {
+      let url = `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json`
+      let cityDataFromAxios = await axios.get(url);
+
+      this.setState({
+        cityData: cityDataFromAxios.data[0],
+        error: false
+      });
+
+    } catch (error) {
+      this.setState({
+        error: true,
+        errorMessage: error.message
+      })
+    }
+  }
+
+  render() {
+    const { cityData, error, errorMessage } = this.state;
+
+    return (
+      <>
+        <h1>API CALLS</h1>
+
+        <CityInput handleCityInput={this.handleCityInput} getCityData={this.getCityData} />
+
+        {
+          error
+            ? <ErrorMessage message={errorMessage} />
+            : <CityData data={cityData} />
+        }
+      </>
+    );
+  }
 }
 
 export default App;
